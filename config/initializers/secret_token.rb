9768545a9d8088c1add6cbf4fe874d6e959207a1
suer <suetsugu.r@gmail.com>
2013-12-21 22:38:01 +0900

    bundle exec rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashbozu2::Application.config.secret_key_base = '9998599f997f60b02f0d18c1c89275118a879501be3636ff0fa27abf78cc0d2d0228358d4fd8e7df3ede3d926311769b2793b81931d786b187fb4f64d0d1e7f9'
