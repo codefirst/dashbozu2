dffbbad9a577c31c6feb19bc1be7a3aa89b3e67f
mallowlabs <mallowlabs@gmail.com>
2013-12-22 15:39:32 +0900

    Added: hook controller

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -3,6 +3,33 @@ class HookController < ApplicationController
   respond_to :json
 
   def hook
-    render :json => {:status => 'ok'}
+    name = params[:name]
+    input_plugin = Dashbozu::Plugin.input[name]
+    unless input_plugin
+      render json: {status: 'error', message: 'name not found'}, status: 404
+      return
+    end
+
+    input_plugin = input_plugin.new
+
+    payload = params[input_plugin.payload]
+    unless payload
+      render json: {status: 'error', message: 'payload not found'}, status: 404
+      return
+    end
+
+    api_key = params[:api_key]
+    project = Project.find_by_api_key api_key if api_key
+    if api_key == nil || project == nil
+      render json: {status: 'error', message: 'invalid api key'}, status: 403
+      return
+    end
+
+    activities = input_plugin.hook(project, payload)
+    activities.each do |activity|
+      activity.save!
+    end
+
+    render json: {status: 'ok'}
   end
 end
--- spec/controllers/hook_controller_spec.rb
+++ spec/controllers/hook_controller_spec.rb
@@ -10,4 +10,28 @@ describe HookController do
 #    it { should eq 404 }
 #  end
 
+  context 'post' do
+    context 'name not found' do
+      before {
+        get :hook, format: 'json', name: 'not_exist'
+      }
+      subject { response.response_code }
+      it { should eq 404 }
+    end
+    context 'payload not found' do
+      before {
+        get :hook, format: 'json', name: 'github'
+      }
+      subject { response.response_code }
+      it { should eq 404 }
+    end
+    context 'api key not found' do
+      before {
+        get :hook, format: 'json', name: 'github', api_key: 'invalid_key', payload: '{}'
+      }
+      subject { response.response_code }
+      it { should eq 403 }
+    end
+  end
+
 end
