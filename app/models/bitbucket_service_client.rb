92b06d69b10936f05341190cd411bbc543f7acb7
suer <suetsugu.r@gmail.com>
2013-12-23 22:30:14 +0900

    show bitbucket private repos

--- app/models/bitbucket_service_client.rb
+++ app/models/bitbucket_service_client.rb
@@ -12,8 +12,7 @@ class BitbucketServiceClient
                               oauth_secret:  @oauth_credentials[:secret],
                               client_id: Settings.omniauth.bitbucket.key,
                               client_secret: Settings.omniauth.bitbucket.secret)
-    public_repos = bitbucket.repos.list.select {|repo| not repo.is_private}
-    public_repos.map do |repo|
+    bitbucket.repos.list.map do |repo|
       Project.new(provider: @provider, name: "#{repo.owner}/#{repo.name}")
     end
   end
