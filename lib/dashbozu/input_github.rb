24650948857cc11b9421e93f69760eb6f3ed0031
mallowlabs <mallowlabs@gmail.com>
2014-01-06 01:09:46 +0900

    Added: support github issue webhook

--- lib/dashbozu/input_github.rb
+++ lib/dashbozu/input_github.rb
@@ -9,6 +9,15 @@ module Dashbozu
     def hook(project, params)
       payload_body = params[:payload]
       json = MultiJson.load(payload_body)
+      if json['before']
+        return hook_push(project, json)
+      elsif json['issue']
+        return hook_issue(project, json)
+      end
+    end
+
+    private
+    def hook_push(project, json)
       repos_name = json['repository']['name']
       json['commits'].map do |c|
         Activity.new(
@@ -19,8 +28,22 @@ module Dashbozu
           author: c['author']['username'],
           icon_url: GravatarImageTag.gravatar_url(c['author']['email']),
           source: 'github'
-       )
+        )
       end
+    end
+
+    def hook_issue(project, json)
+      issue = json['issue']
+      user = issue['user']
+      [Activity.new(
+        project_id: project.id,
+        title: issue['title'],
+        body: issue['body'],
+        url: issue['html_url'],
+        author: user['login'],
+        icon_url: user['avatar_url'],
+        source: 'github'
+      )]
     end
   end
 end
--- spec/lib/input_github_spec.rb
+++ spec/lib/input_github_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Dashbozu::InputGitHub' do
-  context 'hook' do
+  context 'push' do
     before {
 @payload = <<PAYLOAD
 {
@@ -160,6 +160,123 @@ PAYLOAD
       subject { @activities[0] }
       its (:source) { should eq 'github' }
       its (:icon_url) { should =~ /gravatar/ }
+    end
+  end
+
+  context 'issues' do
+    before {
+@payload = <<PAYLOAD
+{
+  "action":"opened",
+  "issue":{
+    "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+    "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+    "number": 1347,
+    "state": "open",
+    "title": "Found a bug",
+    "body": "I'm having a problem with this.",
+    "user": {
+      "login": "octocat",
+      "id": 1,
+      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+      "gravatar_id": "somehexcode",
+      "url": "https://api.github.com/users/octocat",
+      "html_url": "https://github.com/octocat",
+      "followers_url": "https://api.github.com/users/octocat/followers",
+      "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+      "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+      "organizations_url": "https://api.github.com/users/octocat/orgs",
+      "repos_url": "https://api.github.com/users/octocat/repos",
+      "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/octocat/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "labels": [
+      {
+        "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
+        "name": "bug",
+        "color": "f29513"
+      }
+    ],
+    "assignee": {
+      "login": "octocat",
+      "id": 1,
+      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+      "gravatar_id": "somehexcode",
+      "url": "https://api.github.com/users/octocat",
+      "html_url": "https://github.com/octocat",
+      "followers_url": "https://api.github.com/users/octocat/followers",
+      "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+      "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+      "organizations_url": "https://api.github.com/users/octocat/orgs",
+      "repos_url": "https://api.github.com/users/octocat/repos",
+      "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/octocat/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "milestone": {
+      "url": "https://api.github.com/repos/octocat/Hello-World/milestones/1",
+      "number": 1,
+      "state": "open",
+      "title": "v1.0",
+      "description": "",
+      "creator": {
+        "login": "octocat",
+        "id": 1,
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "somehexcode",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+        "followers_url": "https://api.github.com/users/octocat/followers",
+        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+        "organizations_url": "https://api.github.com/users/octocat/orgs",
+        "repos_url": "https://api.github.com/users/octocat/repos",
+        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/octocat/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "open_issues": 4,
+      "closed_issues": 8,
+      "created_at": "2011-04-10T20:09:31Z",
+      "due_on": null
+    },
+    "comments": 0,
+    "pull_request": {
+      "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+      "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+      "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+    },
+    "closed_at": null,
+    "created_at": "2011-04-22T13:33:48Z",
+    "updated_at": "2011-04-22T13:33:48Z"
+  }
+}
+PAYLOAD
+      @project = Project.new
+      @activities = Dashbozu::InputGitHub.new.hook(@project, payload: @payload)
+    }
+    context 'length' do
+      subject { @activities }
+      its (:length) { should eq 1 }
+    end
+    context 'first' do
+      subject { @activities[0] }
+      its (:source) { should eq 'github' }
+      its (:body) { should eq "I'm having a problem with this." }
+      its (:title) { should eq 'Found a bug' }
+      its (:url) { should eq 'https://github.com/octocat/Hello-World/issues/1347' }
+      its (:icon_url) { should eq 'https://github.com/images/error/octocat_happy.gif' }
+      its (:author) { should eq 'octocat' }
     end
   end
 end
